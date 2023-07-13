b21d06c0011c41b67803f476eff18225fbf5c0a1
Jiseok Son <jiseokson@gmail.com>
2023-07-13 14:30:43 +0900

    Add: JWT token refresh API endpoint

--- auths/urls.py
+++ auths/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenRefreshView
 from auths.views import OAuthTokenObtainView
 
 app_name = 'auths'
 
 urlpatterns = [
     path('<str:provider>/token', OAuthTokenObtainView.as_view(), name='token_obtain'),
+    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
